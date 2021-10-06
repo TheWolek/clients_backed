24048c7ce88d23a0e786897b7ddf0f3e0ad590b4
TheWolek <pawelm916@wp.pl>
2021-10-06 22:18:01 +0200

    create client blueprint

--- api.py
+++ api.py
@@ -4,73 +4,13 @@ import json
 
 from DB_query import DB_query
 from user import user_BP
+from client import client_BP
 
 api = Flask(__name__)
 CORS(api)
 
 api.register_blueprint(user_BP, url_prefix='/user')
-
-
-@ api.route("/client", methods=["POST"])  # create new client
-def clientAdd():
-    req = request.json
-
-    # body validation
-    if "imie_Nazwisko" not in req:
-        return Response("{'err_msg':'imie_Nazwisko field is missing'}", status=406, mimetype='application/json')
-
-    # body data parsing
-    name = req["imie_Nazwisko"].lower()
-
-    # DB query
-    sql = "INSERT INTO clients (imie_Nazwisko) VALUES (%s)"
-    val = (name,)
-    res = DB_query(sql, val)
-
-    # something went wrong with database and no record was insterted, throw error
-    if res["status"] != 1:
-        return Response(status=500, mimetype='application/json')
-
-    # OK, client created
-    return Response(json.dumps({"CID": res["ID"]}), status=200, mimetype='application/json')
-
-
-# fetch client data by name
-@ api.route("/client/<client_name>", methods=["GET"])
-def findClient(client_name):
-    client_name = "%"+client_name+"%"
-    sql = "SELECT id, imie_Nazwisko FROM clients WHERE imie_Nazwisko like %s"
-    val = (client_name, )
-    res = DB_query(sql, val)
-
-    if res["status"] != 1:  # if returned status is wrong
-        return Response("{'err_msg':'DB error'}", status=500, mimetype='application/json')
-
-    return Response(json.dumps({"data": res["data"]}), status=200, mimetype='application/json')
-
-# delete client
-
-
-@ api.route("/client/<CID>", methods=["DELETE"])
-def deleteClient(CID: int):
-    # check if user exists
-    sql = "SELECT id FROM clients WHERE ID=%s"
-    val = (CID,)
-    res = DB_query(sql, val)
-
-    if res["status"] != 1:  # if returned status is wrong
-        return Response("{'err_msg':'DB error'}", status=500, mimetype='application/json')
-
-    if res["data"] == []:
-        return Response("{'err_msg':'no client was found'}", status=404, mimetype='application/json')
-
-    sql = "DELETE FROM clients WHERE id=%s"
-    res = DB_query(sql, (CID,))
-
-    if res["status"] != 1:
-        return Response("{'err_msg':'DB error'}", status=500, mimetype='application/json')
-
-    return Response(json.dumps({"CID": CID}), status=200, mimetype='application/json')
+api.register_blueprint(client_BP, url_prefix='/client')
 
 
 @ api.route("/proc", methods=["POST"])
---
+++ client.py
@@ -0,0 +1,67 @@
+from flask import Flask, request, Response
+from flask.blueprints import Blueprint
+import json
+
+from DB_query import DB_query
+
+client_BP = Blueprint("client", __name__)
+
+
+@ client_BP.route("/", methods=["POST"])  # create new client
+def clientAdd():
+    req = request.json
+
+    # body validation
+    if "imie_Nazwisko" not in req:
+        return Response("{'err_msg':'imie_Nazwisko field is missing'}", status=406, mimetype='application/json')
+
+    # body data parsing
+    name = req["imie_Nazwisko"].lower()
+
+    # DB query
+    sql = "INSERT INTO clients (imie_Nazwisko) VALUES (%s)"
+    val = (name,)
+    res = DB_query(sql, val)
+
+    # something went wrong with database and no record was insterted, throw error
+    if res["status"] != 1:
+        return Response(status=500, mimetype='application/json')
+
+    # OK, client created
+    return Response(json.dumps({"CID": res["ID"]}), status=200, mimetype='application/json')
+
+
+# fetch client data by name
+@ client_BP.route("/<client_name>", methods=["GET"])
+def findClient(client_name):
+    client_name = "%"+client_name+"%"
+    sql = "SELECT id, imie_Nazwisko FROM clients WHERE imie_Nazwisko like %s"
+    val = (client_name, )
+    res = DB_query(sql, val)
+
+    if res["status"] != 1:  # if returned status is wrong
+        return Response("{'err_msg':'DB error'}", status=500, mimetype='application/json')
+
+    return Response(json.dumps({"data": res["data"]}), status=200, mimetype='application/json')
+
+
+@ client_BP.route("/<CID>", methods=["DELETE"])  # delete client
+def deleteClient(CID: int):
+    # check if user exists
+    sql = "SELECT id FROM clients WHERE ID=%s"
+    val = (CID,)
+    res = DB_query(sql, val)
+
+    if res["status"] != 1:  # if returned status is wrong
+        return Response("{'err_msg':'DB error'}", status=500, mimetype='application/json')
+
+    if res["data"] == []:
+        return Response("{'err_msg':'no client was found'}", status=404, mimetype='application/json')
+
+    sql = "DELETE FROM clients WHERE id=%s"
+    res = DB_query(sql, (CID,))
+
+    if res["status"] != 1:
+        return Response("{'err_msg':'DB error'}", status=500, mimetype='application/json')
+
+    return Response(json.dumps({"CID": CID}), status=200, mimetype='application/json')
